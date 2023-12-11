c8b1036fdf534845c393c681d11ec96747f16fda
bill sukh <bill.sukh97@gmail.com>
2023-12-10 22:04:18 -0600

    Added react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className='text-red-500'>Hello World!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Profile' element={<Home />} />
+        <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/SignUp' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
- 
