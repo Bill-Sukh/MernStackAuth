6d5d3f0f2150b79f02190e9335a94443072ac4f2
bill sukh <bill.sukh97@gmail.com>
2023-12-17 22:12:05 -0600

    react header added and created api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Profile' element={<Home />} />
-        <Route path='/SignIn' element={<SignIn />} />
-        <Route path='/SignUp' element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Profile' element={<Home />} />
+          <Route path='/SignIn' element={<SignIn />} />
+          <Route path='/SignUp' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
