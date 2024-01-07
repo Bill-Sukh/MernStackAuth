4089490cce29d126326ce3d30474df54a1f828ea
bill sukh <bill.sukh97@gmail.com>
2024-01-07 00:51:42 -0600

    Sign up controller code completed and api error handling added

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js"
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js"
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const { userName, email, password} = req.body
-    const newUser = new User({ userName, email, password})
-    await newUser.save()
-    res.status(201).json({ message: "Signed up successfully!"})
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ userName, email, password: hashedPassword})
+    try {
+        await newUser.save()
+        res.status(201).json({ message: "Signed up successfully!"})
+    } catch (error) {
+        next(error)
+    }   
 }
